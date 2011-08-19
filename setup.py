5bd535d3c85e2a53f6f63232fc528ecf4b2f9d70
Brian Luft <brian@unbracketed.com>
2011-08-19 00:08:32 -0700

    pull over code from redis-import-tools

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='RedLine',
+    version='0.1',
+    long_description=__doc__,
+    packages=['redline'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=['redis>=2.2.0'],
+    scripts=['bin/redis-import-set']
+)
