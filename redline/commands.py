4f3175e2af607ba8570134d1742c9c94a3ba34bf
Brian Luft <brian@unbracketed.com>
2011-08-28 21:28:03 -0700

    using subparsers for subcommands; load functions moved to RedisImport class

--- redline/commands.py
+++ redline/commands.py
@@ -6,57 +6,54 @@ from itertools import groupby
 import redis
 
 
-def load_set(key, IN, **kwargs):
-    """
-    """
-    r = redis.Redis()
-    pipeline_redis = r.pipeline()
-    count = 0
-    batch_size = kwargs.get('batch_size', 1000)
+class RedisImport(object):
 
-    seen = set([None])
-    reader_files = reader(IN, delimiter='\t')
-    for member, _ in groupby(reader_files,
-    lambda x: x[0] if len(x) else None):
-        if member not in seen:
-            pipeline_redis.sadd(key, member.rstrip())
+    def __init__(self, **kwargs):
+    	self.redis = redis.Redis()
+    	self.pipeline = self.redis.pipeline()
+    	self.batch_size = kwargs.get('batch_size', 1000)
+
+    def load_set(self, key, IN, **kwargs):
+        """
+        """
+        seen = set([None])
+        reader_files = reader(IN, delimiter='\t')
+
+        #TODO support multiple member add for Redis 2.4+
+        for i, (member, _) in enumerate(groupby(reader_files,
+        lambda x: x[0] if len(x) else None)):
+            if member not in seen:
+                self.pipeline.sadd(key, member.rstrip())
+                seen.add(member)
+                if not i % self.batch_size:
+                    self.pipeline.execute()
+        #send the last batch
+        self.pipeline.execute()
+
+
+    def load_list(self, key, IN, **kwargs):
+        """
+        """
+        count = 0
+
+        for line in IN:
+            self.pipeline.rpush(key, line.rstrip())
             count += 1
-            seen.add(member)
-            if not count % batch_size:
-                pipeline_redis.execute()
-    #send the last batch
-    pipeline_redis.execute()
+            if not count % self.batch_size:
+                self.pipeline.execute()
+        #send the last batch
+        self.pipeline.execute()
 
 
-def load_list(key, IN, **kwargs):
-    """
-    """
-    r = redis.Redis()
-    pipeline_redis = r.pipeline()
-    count = 0
-    batch_size = kwargs.get('batch_size', 1000)
+    def load_hash_list(IN, **kwargs):
+        """
+        """
+        count = 0
 
-    for line in IN:
-        pipeline_redis.rpush(key, line.rstrip())
-        count += 1
-        if not count % batch_size:
-            pipeline_redis.execute()
-    #send the last batch
-    pipeline_redis.execute()
-
-
-def load_hash_list(IN, **kwargs):
-    """
-    """
-    r = redis.Redis()
-    pipeline_redis = r.pipeline()
-    count = 0
-    batch_size = kwargs.get('batch_size', 1000)
-
-    for key, mapping in IN:
-        pipeline_redis.hmset(key, mapping)
-        count += 1
-        if not count % batch_size:
-            pipeline_redis.execute()
-    #send the last batch
-    pipeline_redis.execute()
+        for key, mapping in IN:
+            self.pipeline.hmset(key, mapping)
+            count += 1
+            if not count % self.batch_size:
+                self.pipeline.execute()
+        #send the last batch
+        pipeline.execute()
